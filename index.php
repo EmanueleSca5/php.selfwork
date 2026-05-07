3a42052cbb7ef1f7347819cf32430099ffa82968
EmanueleSca5 <emanuele.scagliarini05@gmail.com>
2026-05-07 12:30:15 +0200

    php_01_emanuele_scagliarini

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ var_dump($float);
 var_dump($String);
 var_dump($boolean);
 
-define(INTEGER, $integer);
-define(FLOAT, $float);
-define(STRING, $String);
-define(BOOLEAN, $boolean);
+define("INT", $integer);
+define("FLOAT", $float);
+define("STRING", $String);
+define("BOOLEAN", $boolean);
